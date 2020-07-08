9b2e971a157cf49babfc6944d36b6d2fd7938708
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-08 10:59:44 +0700

    Initial commit

---
+++ src/main/java/sia/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,12 @@
+package sia.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+}
