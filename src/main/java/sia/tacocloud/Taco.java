fb2e3d3090b5132f2d5b312aa5a61916a399c24e
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-09 09:15:00 +0700

    Add validate for input field

--- src/main/java/sia/tacocloud/Taco.java
+++ src/main/java/sia/tacocloud/Taco.java
@@ -5,10 +5,17 @@ package sia.tacocloud;
 import java.util.List;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 chracters long.")
     private String name;
+
+    @Size(min=1, message="You must at least 1 ingredient.")
     private List<String> ingredients;
 
 }
